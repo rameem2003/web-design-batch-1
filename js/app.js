76cf57eb6485455990c7a735a6f7444366c71595
Mahmood Hassan Rameem <85950459+rameem2003@users.noreply.github.com>
2025-09-07 16:27:05 +0000

    js

---
+++ js/app.js
@@ -0,0 +1,24 @@
+var data = null
+var name = "fcwefvwe"
+console.log(name);
+
+// data = "wmewewpe"
+// data = "rameem"
+name = "Shahina"
+console.log(name)
+
+
+var age = 100
+var age
+console.log(age);
+
+
+
+
+
+
+// string "rameem"
+// number 100, 20000000000, 45.5898
+// object {}
+// array []
+// boolean true / false
