ed69932bd7d2bea4eb4f6d53cd28a0a93ed8e7fa
Mahmood Hassan Rameem <85950459+rameem2003@users.noreply.github.com>
2025-09-10 16:12:05 +0000

    update

--- js/app.js
+++ js/app.js
@@ -1,24 +1,67 @@
-var data = null
-var name = "fcwefvwe"
-console.log(name);
+// var vs let vs const
 
-// data = "wmewewpe"
-// data = "rameem"
-name = "Shahina"
-console.log(name)
+// declare
+// re declare
+// assign
+// re assign
+
+// fucntion scope -> var
+// block scope
+// var name = "rameem"
+
+// function abc(){
+//     name = "shahina"
+//     console.log(name);
+//     console.log(10000)
+// }
+
+// console.log(name);
+
+// abc()
+
+// let / const
+// console.log(name);
+
+// let এ redeclare করা যায় না কিন্তু re assign করা যায়
+// let block scope
+// let name = "rameem";
+// name = "Shahina";
+
+// function efg() {
+//   const baby = "ayat";
+//   console.log(baby);
+// }
 
 
-var age = 100
-var age
-console.log(age);
+// const baby = "mariam"
+// efg()
+// console.log(baby);
+
+
+// const
+// একে আপনি re declare বা re assign কোনটাই  করতে পারবেন না
+// const f_name = "raeem"
+
+// console.log(f_name);
+
+let name = "rameem"
+let age  =22
+let gender = "male"
+
+let name1 = "Shahina"
+let age1 = 30
+let gender1 = "female"
+
+const object = {
+    // key : value
+    name : "Shahina",
+    age : 30,
+    gender : "female"
+}
+
+ object.phone = "0154154"
+ object.age = 35
 
 
 
-
-
-
-// string "rameem"
-// number 100, 20000000000, 45.5898
-// object {}
-// array []
-// boolean true / false
+console.log(object)
