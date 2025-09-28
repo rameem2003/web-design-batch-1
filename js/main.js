60b4f3bf64d9f2150136aa18b78092cefa8acea1
Mahmood Hassan Rameem <rameem2019@gmail.com>
2025-09-28 21:59:59 +0600

    update

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// let object = {
+//   name: "Rameem",
+//   age: 22,
+//   phone: "1234567890",
+//   food: ["agag", "qwefwefoi"],
+//   status: true,
+//   eat: function () {
+//     console.log("eating");
+//   },
+// };
+
+// const { name, age, phone, eat } = object;
+
+// object.eat();
+
+// let country = "Bangladesh";
+
+// let newString = country.split("");
+// let joinstring = newString.join("");
+
+// console.log(joinstring);
+
+let array = [
+  { name: "Alice", age: 25, status: "active" },
+  { name: "Bob", age: 30, status: "inactive" },
+  { name: "Charlie", age: 22, status: "active" },
+  { name: "Diana", age: 28, status: "active" },
+  { name: "Ethan", age: 35, status: "inactive" },
+  { name: "Fiona", age: 27, status: "active" },
+  { name: "George", age: 32, status: "inactive" },
+  { name: "Hannah", age: 24, status: "active" },
+  { name: "Ian", age: 29, status: "inactive" },
+  { name: "Julia", age: 26, status: "active" },
+];
+
+let newArray = array.slice(0, 5);
+
+console.log(array.length);
