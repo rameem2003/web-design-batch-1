d7e2b96db9f353b845f94381c17d544e467d2397
Mahmood Hassan Rameem <rameem2019@gmail.com>
2025-10-19 22:15:25 +0600

    todo app

--- app.js
+++ app.js
@@ -1,38 +1,72 @@
-// const btn = document.getElementById("btn");
-// const navMenu = document.getElementById("nav-menu");
-
-// btn.addEventListener("click", () => {
-//   //   console.log("click");
-
-//   navMenu.classList.toggle("show");
-// });
-
-// let firstNumber = prompt("Enter first number ");
-// let secondNumber = prompt("Enter second number ");
-
-// console.log(typeof firstNumber);
-
-// let sum = parseInt(firstNumber) + parseInt(secondNumber);
-// console.log(sum);
-
-let array = [];
-
+let array = JSON.parse(localStorage.getItem("todo-app")) || [];
+let index = null;
+console.log(array);
+// submit form elements
+const submit = document.getElementById("main-form");
 const name = document.getElementById("name");
 const userEmail = document.getElementById("email");
 const submitBtn = document.getElementById("submit");
+// update form elements
+const update = document.getElementById("update");
+const updateName = document.getElementById("update-name");
+const updateEmail = document.getElementById("update-email");
+const updateSubmitBtn = document.getElementById("update-submit");
+// list element
 const ul = document.getElementById("list");
+let updateData = {};
 
+// delete item
+const deleteItem = (i) => {
+  console.log("click", i);
+  array.splice(i, 1);
+  localStorage.setItem("todo-app", JSON.stringify(array));
+  fetchData();
+};
+
+// update item (resubmit form open function)
+const updateItem = (i) => {
+  index = i;
+  submit.style.display = "none";
+  update.style.display = "block";
+
+  updateName.value = array[i].name;
+  updateEmail.value = array[i].email;
+};
+
+// edit todo function (After resubmit form)
+const edittodo = () => {
+  let updateTodo = {
+    name: updateName.value,
+    email: updateEmail.value,
+  };
+
+  array[index].name = updateTodo.name;
+  array[index].email = updateTodo.email;
+
+  localStorage.setItem("todo-app", JSON.stringify(array));
+  fetchData();
+
+  update.style.display = "none";
+  submit.style.display = "block";
+};
+
+// fetch data function
 const fetchData = () => {
+  update.style.display = "none";
   ul.innerHTML = "";
   array.map((item, i) => {
     let li = document.createElement("li");
     li.innerHTML = `<p>${item.name}</p>
-    <p>${item.email}</p>`;
+    <p>${item.email}</p>
+    <button onclick="deleteItem(${i})">Delete</button>
+    <button onclick="updateItem(${i})">Update</button>
+    `;
 
     ul.appendChild(li);
   });
 };
 
+// submit form event listener
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
   let data = {
@@ -41,9 +75,15 @@ submitBtn.addEventListener("click", (e) => {
   };
 
   array.push(data);
-
+  localStorage.setItem("todo-app", JSON.stringify(array));
   console.log(array);
   fetchData();
+});
+
+// update form event listener
+updateSubmitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  edittodo();
 });
 
 fetchData();
