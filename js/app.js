5be8fe78ab372fbe3c07b886300b9e63c5c3a717
Mahmood Hassan Rameem <rameem2019@gmail.com>
2025-11-02 22:05:20 +0600

    navbar  compete

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const nav_btn = document.getElementById("nav-btn");
+const mobile_menu = document.getElementById("mobile_menu");
+const mobile_list = document.getElementById("mobile_list");
+const option1 = document.getElementById("option1");
+const sub_category_1 = document.getElementById("sub-category-1");
+const option2 = document.getElementById("option2");
+const sub_category_2 = document.getElementById("sub-category-2");
+
+nav_btn.addEventListener("click", () => {
+  mobile_menu.classList.toggle("active");
+  //   mobile_list.classList.toggle("opacity-100");
+});
+
+option1.addEventListener("click", () => {
+  sub_category_1.classList.toggle("active");
+  sub_category_1.classList.toggle("h-full");
+});
+option2.addEventListener("click", () => {
+  sub_category_2.classList.toggle("active");
+});
